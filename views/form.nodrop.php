62dbb61deee93a08721906a5a8271e4fbcb38e71
Brian McCann <bmccann@forsythtech.edu>
2014-10-10 11:17:54 -0400

    1.0.2

--- views/form.nodrop.php
+++ views/form.nodrop.php
@@ -2,7 +2,7 @@
         <div class="large-16 columns">
             <h4>Not eligible to drop courses.</h4>
             <p>Your account indicates you are a Middle College or an Early College student.</p>
-            <p>If you are a Middle/Early College student, pease contact your liaison for assistance with dropping courses.</p>
+            <p>If you are a Middle/Early College student, please contact your liaison for assistance with dropping courses.</p>
             <p>If you are no longer a Middle/Early College student, please contact the <a href="http://www.forsythtech.edu/services-students/student-resources/academic-records-transcripts/">Records Office</a> for further assistance.</p>
         </div>
     </div>
